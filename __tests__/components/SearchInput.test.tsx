b1042f21b8ff8d7a4ae1e1a52dabe6143a4ca6c5
Lucas Lauriano de Souza <lucaslauriano.souza@gmail.com>
2023-08-21 18:26:21 -0300

    feat: implementing tests

---
+++ __tests__/components/SearchInput.test.tsx
@@ -0,0 +1,20 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import SearchInput from '@/app/components/SearchInput';
+
+describe('SearchInput component', () => {
+  test('should update the search term correctly when input value changes', () => {
+    const onSearch = jest.fn();
+    render(<SearchInput onSearch={onSearch} />);
+    const input = screen.getByTestId('desktop-search-job');
+    fireEvent.change(input, { target: { value: 'test' } });
+    expect(input.value).toBe('test');
+  });
+  test('should call the onSearch callback with the correct search term when input value changes', () => {
+    const onSearch = jest.fn();
+    render(<SearchInput onSearch={onSearch} />);
+    const input = screen.getByTestId('desktop-search-job');
+    fireEvent.change(input, { target: { value: 'test' } });
+
+    expect(onSearch).toHaveBeenCalledWith('test');
+  });
+});
