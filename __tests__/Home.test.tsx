f20358674944660f0ab1b924e736f606c5077ce4
Lucas Lauriano de Souza <lucaslauriano.souza@gmail.com>
2023-08-22 00:19:52 -0300

    feat: add unit tests for the home component

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,19 +1,37 @@
-import { fireEvent, render, screen } from '@testing-library/react';
 import Home from '@/app/page';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 
 describe('Home Page ', () => {
-  describe('Rendering', () => {
-    it('should have heading text', async () => {
-      render(<Home />);
+  test('should have heading text', async () => {
+    render(<Home />);
 
-      const heading = screen.getByRole('heading', {
-        name: /Find the best jobs./i,
-      });
-      fireEvent.click(screen.getByText('Visit the job posting page'));
-      await screen.findByRole('heading');
-
-      expect(heading).toBeInTheDocument();
-      expect(screen.getByText(/Find the best jobs./i)).toBeInTheDocument();
+    const heading = screen.getByRole('heading', {
+      name: /Find the best jobs./i,
     });
+    fireEvent.click(screen.getByText('Visit the job posting page'));
+    await screen.findByRole('heading');
+
+    expect(heading).toBeInTheDocument();
+    expect(screen.getByText(/Find the best jobs./i)).toBeInTheDocument();
+  });
+
+  test('renders the "Visit the job posting page" link', () => {
+    render(<Home />);
+
+    const link = screen.getByRole('link', {
+      name: /Visit the job posting page/i,
+    });
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute('href', '/jobs');
+  });
+
+  test('applies correct styling to the link', () => {
+    render(<Home />);
+
+    const link = screen.getByRole('link', {
+      name: /Visit the job posting page/i,
+    });
+    expect(link).toHaveClass('bg-white/10');
+    expect(link).toHaveClass('text-white');
   });
 });
