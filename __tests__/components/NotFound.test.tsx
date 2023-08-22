e0cc76dad77e4461cb0ee6e89fe605b3c1c977b9
Lucas Lauriano de Souza <lucaslauriano.souza@gmail.com>
2023-08-22 00:32:27 -0300

    Feat/implementing tests  (#6)
    * hotfix: small changes
    
    * feat: implementing tests for the about component
    
    * feat: implementing tests for the Footer component
    
    * feat: implementing tests for NotFound component
    
    * feat: add tests for the search input component
    
    * feat: add unit tests for the home component

---
+++ __tests__/components/NotFound.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '@/app/components/NoFound';
+
+describe('NotFound Component', () => {
+  it('should renders the 404 text', () => {
+    render(<NotFound />);
+    const notFoundText = screen.getByText(/404/i);
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  it('should renders the page not found heading', () => {
+    render(<NotFound />);
+    const heading = screen.getByText(/Page not found/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('should renders the page not found message', () => {
+    render(<NotFound />);
+    const message = screen.getByText(
+      /Sorry, we couldn’t find the page you’re looking for./i
+    );
+    expect(message).toBeInTheDocument();
+  });
+
+  it('should renders the "Go back home" link', () => {
+    render(<NotFound />);
+    const link = screen.getByRole('link', { name: /Go back home/i });
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute('href', '#');
+  });
+
+  it('applies correct styling to the "Go back home" link', () => {
+    render(<NotFound />);
+    const link = screen.getByRole('link', { name: /Go back home/i });
+    expect(link).toHaveClass('bg-indigo-600');
+    expect(link).toHaveClass('text-white');
+  });
+});
--- __tests__/components/SearchInput.test.tsx
+++ __tests__/components/SearchInput.test.tsx
@@ -2,20 +2,62 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import SearchInput from '@/app/components/SearchInput';
 
 describe('SearchInput component', () => {
+  test('should calls onSearch prop when input changes', () => {
+    const mockOnSearch = jest.fn();
+    render(<SearchInput onSearch={mockOnSearch} />);
+
+    const inputElement = screen.getByTestId('mobile-search-job');
+    fireEvent.change(inputElement, { target: { value: 'test' } });
+
+    expect(mockOnSearch).toHaveBeenCalledTimes(1);
+    expect(mockOnSearch).toHaveBeenCalledWith('test');
+  });
+
+  test('displays mobile input by default', () => {
+    render(<SearchInput onSearch={() => {}} />);
+
+    const mobileInput = screen.getByTestId('mobile-search-job');
+    const desktopInput = screen.queryByTestId('desktop-search-job');
+
+    expect(mobileInput).toBeInTheDocument();
+    expect(desktopInput).toBeInTheDocument();
+  });
+
   test('should update the search term correctly when input value changes', () => {
-    const onSearch = jest.fn();
-    render(<SearchInput onSearch={onSearch} />);
+    const mockOnSearch = jest.fn();
+    render(<SearchInput onSearch={mockOnSearch} />);
     const input = screen.getByTestId('desktop-search-job');
     fireEvent.change(input, { target: { value: 'test' } });
     expect(input.value).toBe('test');
   });
 
   test('should call the onSearch callback with the correct search term when input value changes', () => {
-    const onSearch = jest.fn();
-    render(<SearchInput onSearch={onSearch} />);
+    const mockOnSearch = jest.fn();
+    render(<SearchInput onSearch={mockOnSearch} />);
     const input = screen.getByTestId('desktop-search-job');
     fireEvent.change(input, { target: { value: 'test' } });
 
-    expect(onSearch).toHaveBeenCalledWith('test');
+    expect(mockOnSearch).toHaveBeenCalledWith('test');
+  });
+
+  test('displays desktop input when screen size is not small', () => {
+    Object.defineProperty(window, 'innerWidth', {
+      value: 1024,
+      writable: true,
+    });
+
+    render(<SearchInput onSearch={() => {}} />);
+
+    const mobileInput = screen.queryByTestId('mobile-search-job');
+    const desktopInput = screen.getByTestId('desktop-search-job');
+
+    expect(mobileInput).toBeInTheDocument();
+    expect(desktopInput).toBeInTheDocument();
+
+    // Resetting the window.innerWidth to its original value
+    Object.defineProperty(window, 'innerWidth', {
+      value: global.innerWidth,
+      writable: false,
+    });
   });
 });
